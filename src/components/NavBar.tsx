23aeca1f58530315db49192d43df5e603b55c106
cristianocsl <cslcristiano@gmail.com>
2022-04-27 15:04:53 -0300

    feat: adiciona header com link para retornar ao home

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  useColorModeValue,
+} from '@chakra-ui/react';
+// import { useNavigate } from 'react-router-dom';
+
+
+// const Links = ['Home'];
+
+// const NavLink = ({ children }: { children: ReactNode }) => {
+//   // const navigate = useNavigate();
+//   return (
+//   <Link
+//     px={2}
+//     py={1}
+//     rounded={'md'}
+//     _hover={{
+//       textDecoration: 'none',
+//       bg: useColorModeValue('gray.200', 'gray.700'),
+//     }}
+//     href={'/'}>
+//     {children}
+//   </Link>
+// )};
+
+export default function Simple() {
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack spacing={8} alignItems={'center'}>
+            <RouterLink to='/'>
+              <Link
+                px={2}
+                py={1}
+                rounded={'md'}
+                _hover={{
+                  textDecoration: 'none',
+                  bg: useColorModeValue('gray.200', 'gray.700'),
+                }}
+              >
+                Home
+              </Link>
+            </RouterLink>
+          </HStack>
+          <Flex alignItems={'center'}>
+                <Avatar
+                  size={'sm'}
+                  src={
+                    'https://www.freeiconspng.com/uploads/pokeball-icon-1.png'
+                  }
+                />
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
