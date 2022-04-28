ae5f3c317e2b22c25a11a908b0040c5f0229aff5
cristianocsl <cslcristiano@gmail.com>
2022-04-27 21:18:27 -0300

    refactor: muda exportação para export default

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './theme';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider resetCSS theme={ theme }>
       <App />
     </ChakraProvider>
   </React.StrictMode>
